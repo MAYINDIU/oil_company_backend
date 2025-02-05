849e40585d83ba8bc01b183e89161f4706a75749
Prime Islami Life Insruance Ltd. <mayindiu@gmail.com>
2025-02-05 12:10:22 +0600

    toromba

---
+++ models/torombaModel.js
@@ -0,0 +1,72 @@
+// suplier.js
+const db = require("../config/db");
+const dotenv = require("dotenv");
+
+function getAllToromba(callback) {
+    const query = `
+        SELECT 
+            t.station_id,
+            t.fuel_type,
+            t.torombo_no,
+            t.torombo_op_b,
+            t.torombo_close_balance,
+            t.created_date,
+            t.updated_date,
+            b.branch_name
+        FROM toromba t
+        LEFT JOIN branch b ON t.station_id = b.branch_id
+    `;
+
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error("Error fetching toromba data:", err);
+            return callback(err, null);
+        }
+        callback(null, results);
+    });
+}
+
+
+function createToromba(toromba, callback) {
+    // Fix the variable name from `error` to `err`
+    db.query("INSERT INTO toromba  SET ?", toromba, (err, results) => {
+      if (err) {
+        // Fix the variable name from `err` to `error`
+        callback(err, null);
+        return;
+      } else {
+        callback(null, results);
+      }
+    });
+  }
+
+
+function updateToromba(toromba_id , data, callback) {
+    const updateQuery =
+      "UPDATE toromba  SET " +
+      Object.keys(data)
+        .map((key) => `${key} = ?`)
+        .join(", ") +
+      " WHERE toromba_id  = ?";
+    const updateValues = [...Object.values(data), toromba_id ];
+    // console.log("User Service", updateQuery, updateValues);
+    db.query(updateQuery, updateValues, (err, rows) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      callback(null, rows);
+    });
+  }
+
+
+  module.exports = {
+    createToromba,
+    getAllToromba,
+    updateToromba
+  };
+
+
+
+
+
---
+++ routes/torombaRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const torombaController = require("../controllers/torombaController");
+const verifyToken = require('../utilities/verifyToken');
+const router = express.Router();
+
+// Prefix all routes with '/api'
+router.get("/all-toromba", torombaController.getAllToromba);
+router.post("/create-toromba", torombaController.createToromba);
+router.patch("/update-toromba/:toromba_id", torombaController.updateToromba);
+
+
+
+
+module.exports = router;
