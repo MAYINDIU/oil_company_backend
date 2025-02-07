85fb37675edd810bd9a6456596824b738767e6c7
MuhammadBorhan <0317371981@student.wub.edu.bd>
2025-02-07 22:15:27 +0600

    up to date borhan

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const routes = [
     route: require("./modules/Accounts/SubGroups/SubGroup.route"),
   },
   { path: "/api", route: require("./modules/Accounts/Ledger/Ledger.rotue") },
+  { path: "/api", route: require("./modules/Accounts/Vouchers/Voucher.route") },
 ];
 
 routes.forEach(({ path, route }) => app.use(path, route));
---
+++ modules/Accounts/Vouchers/Voucher.route.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const voucherController = require("./Voucher.controller");
+
+router
+  .post("/auto_journal_post", voucherController.createData)
+  .get("/auto_journal_all", voucherController.getAllData)
+  .get("/journalByDate", voucherController.journalByDateRange)
+  .get("/journalByLedgerDate", voucherController.journalByLedgerDateRange)
+  .get("/partyLedger", voucherController.partyLedgerController)
+  .get("/cus_balance", voucherController.customerOpeningController)
+  .get("/received_payment", voucherController.receivedAndPaymentController)
+  .get("/cash_book", voucherController.cashBookController)
+  .get("/bank_book", voucherController.bankBookController)
+  .get("/auto_journal/:id", voucherController.getDataById)
+  .get("/auto_journal_vc_no", voucherController.getDataByVcNo)
+  .patch("/auto_journal_edit/:journal_id", voucherController.updateDataById)
+  .patch("/voucher_update", voucherController.updateDataByVoucherNo)
+  .patch(
+    "/voucher_unpost_update/:vc_no",
+    voucherController.updatePostedDataByVoucherNo
+  )
+  .delete("/voucher_delete/:vc_no", voucherController.deleteDataByVoucherNo)
+  .delete("/auto_journal_remove/:id", voucherController.deleteDataById);
+
+module.exports = router;
