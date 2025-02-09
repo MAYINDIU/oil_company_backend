01fe5176db253221f472b68d75d93fa17afa8ecb
MD. KHORSHED ALAM <khorshedalam.kbd@gmail.com>
2025-02-09 23:50:33 +0600

    up to date khorshed

--- modules/Users/Users.route.js
+++ modules/Users/Users.route.js
@@ -8,7 +8,7 @@ router.get("/allUsers", userController.getAllUsers);
 router.get("/getUser/:id", userController.getSingleUser);
 router.post("/register", userController.register);
 router.post("/signin", userController.login);
-router.patch("/update-user/:id", verifyToken, userController.updateUser);
+router.patch("/update-user/:id", userController.updateUser);
 router.put(
   "/check-password/:id",
   verifyToken,
