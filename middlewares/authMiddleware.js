24166c8e0039e0ccf264a8e2d376355289e87576
MuhammadBorhan <0317371981@student.wub.edu.bd>
2025-02-11 11:53:04 +0600

    no changed

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+const { verifyToken } = require("../utilities/jwt.js");
+
+const authMiddleware = (roles = []) => {
+  return (req, res, next) => {
+    const authHeader = req.headers?.authorization;
+    console.log(req, res);
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+      const decoded = verifyToken(token);
+      req.user = decoded;
+
+      if (roles.length && !roles.includes(req.user.emptype)) {
+        return res.status(403).json({
+          error: "Forbidden: You do not have access to this resource",
+        });
+      }
+
+      next();
+    } catch (err) {
+      res.status(401).json({ error: "Invalid token" });
+    }
+  };
+};
+
+module.exports = authMiddleware;
